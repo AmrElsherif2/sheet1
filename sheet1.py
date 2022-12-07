a979a914c4e90a140f7c08b9fd1167fa8cb27caa
Eng Amr Elsherif <110961643+AmrElsherif2@users.noreply.github.com>
2022-12-08 00:05:19 +0200

    commit ignore

---
+++ sheet1.py
@@ -0,0 +1,13 @@
+Address = "Menoufia Shebin CIty"
+print(Address[-0])
+print(Address[3])
+print(Address[0:3])
+print(Address[3:20])
+print(Address[:3])
+print(Address[3:])
+print(Address[-1])
+print(Address[-2:])
+print(Address[:-2])
+print(Address[3:20:2])
+
+
